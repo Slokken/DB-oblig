d13c2ee9150679a0c9306f5b491eb307c3fdf87f
Simen Løkken <simen.lokken@gmail.com>
2012-09-20 21:03:29 +0200

    Added a bunch of crap

--- SettInnKundeInfo.php
+++ SettInnKundeInfo.php
@@ -2,10 +2,10 @@
 <?php
 	
 	
-	// $fornavn = htmlspecialchars($_POST['fornavn');
-	// $etternavn = htmlspecialchars($_POST['etternavn']);
-	// $epost = htmlspecialchars($_POST['epost']);
-	// $postnummer = htmlspecialchars($_POST['postnummer']);
+	$fornavn = htmlspecialchars($_POST['fornavn']);
+	$etternavn = htmlspecialchars($_POST['etternavn']);
+	$epost = htmlspecialchars($_POST['e-post']);
+	$postnummer = htmlspecialchars($_POST['postnummer']);
 	
 	//Lage PDO
 	$db = new PDO(
@@ -13,29 +13,38 @@
        . 'dbname=test;charset=UTF-8',
          'root', '');
 		 
-	$db->setAttribute(PDO::ATTR_ERRMODE,
-                  PDO::ERRMODE_EXCEPTION);
+	// $db->setAttribute(PDO::ATTR_ERRMODE,
+                  // PDO::ERRMODE_EXCEPTION);
 
-	$db->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND,
-                  "SET NAMES utf8");	
+	// $db->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND,
+                  // "SET NAMES utf8");	
 	
 	// INSERT**********
 	$stmt = $db->prepare(
          "INSERT INTO kunde (fornavn, etternavn, ePost, postnummer) "
        . "VALUES(:fornavn, :etternavn, :e-post, :postnummer)");
+	   
+	// $stmt->bindParam(':fornavn'		, $_POST['fornavn'], PDO::PARAM_STR, 128);
+	// $stmt->bindParam(':etternavn'	, $_POST['etternavn'], PDO::PARAM_STR, 128);
+	// $stmt->bindParam(':e-post'		, $_POST['e-post'], PDO::PARAM_STR, 128);
+	// $stmt->bindParam(':postnummer'	, $_POST['postnummer'], PDO::PARAM_INT);
 	
-	$stmt->execute(array(	':fornavn' 		=> $_POST['fornavn'],
-							':etternavn'	=> $_POST['etternavn'],
-							':e-post' 		=> $_POST['e-post'],
-							':postnummer' 	=> $_POST['postnummer']));
+	// $stmt->execute();
 	
+	// $stmt->execute(array(	':fornavn' 		=> $_POST['fornavn'],
+							// ':etternavn'	=> $_POST['etternavn'],
+							// ':e-post' 		=> $_POST['e-post'],
+							// ':postnummer' 	=> $_POST['postnummer']));
+							
+							
+	$stmt->execute(array(	':fornavn' 		=> $fornavn,
+							':etternavn'	=> $etternavn,
+							':e-post' 		=> $epost,
+							':postnummer' 	=> $postnummer));
 	
 	// Hente ut igjen kundeID fra den nye kunden
 	// $newID = db->lastInsertId();
 	// echo "KundeID: " . $newID;
-	
-	
-	
 	
  
 ?>
